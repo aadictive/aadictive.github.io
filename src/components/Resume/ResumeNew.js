e79aa90f3110461ff77f596c7512141b2319b6b7
Aditya Chaudhari <asc4896@gmail.com>
2022-10-19 20:19:21 -0400

    Update Resume URL

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "../../Assets/Resume_Aditya_Chaudhari.pdf";
+  "https://raw.githubusercontent.com/aadictive/aadictive.github.io/master/src/Assets/Resume_Aditya_Chaudhari.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
